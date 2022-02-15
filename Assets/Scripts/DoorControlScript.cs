4dd272cd262af18992b1e84362910887957f2b7b
Ricardo Fernández <ricfdzgon@gmail.com>
2022-02-15 18:02:41 +0100

    Pruebas

--- Assets/Scripts/DoorControlScript.cs
+++ Assets/Scripts/DoorControlScript.cs
@@ -6,27 +6,30 @@ public class DoorControlScript : MonoBehaviour
 {
 
     public GameObject[] colorDials;
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(CodeCheck()) {
+        if (CodeCheck())
+        {
             Open();
         }
     }
 
-    private bool CodeCheck() {
-        for(int i=0; i<colorDials.Length; i++) {
-            if (colorDials[i].GetComponentInChildren<SnapTurnDial>() == null) {
+    private bool CodeCheck()
+    {
+        for (int i = 0; i < colorDials.Length; i++)
+        {
+            if (colorDials[i].GetComponentInChildren<SnapTurnDial>() == null)
+            {
                 Debug.Log("DoorControlScript.Open null SpanTurnDial " + i);
                 return false;
             }
-            if( colorDials[i].GetComponentInChildren<SnapTurnDial>().CodeCheck() == false) {
+            if (colorDials[i].GetComponentInChildren<SnapTurnDial>().CodeCheck() == false)
+            {
                 return false;
             }
         }
@@ -34,7 +37,8 @@ public class DoorControlScript : MonoBehaviour
 
     }
 
-    private void Open() {
+    private void Open()
+    {
         Debug.Log("DoorControlScript.Open A abriríase se souperamos como");
     }
 }
