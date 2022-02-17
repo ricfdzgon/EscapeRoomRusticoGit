600fb395b492536cd8652528b01f4836504ed975
Ricardo Fernández <ricfdzgon@gmail.com>
2022-02-17 18:42:02 +0100

    Retoques código para coger color automáticamente

--- Assets/Scripts/PuzzlePieceColor.cs
+++ Assets/Scripts/PuzzlePieceColor.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PuzzlePieceColor : MonoBehaviour
 {
+    public Material color;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Chapa1" || child.name == "Chapa2" || child.name == "Chapa3")
+            {
+                child.GetComponent<MeshRenderer>().material = color;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
