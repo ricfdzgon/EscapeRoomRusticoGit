beedb402562992790ba553a9afc54f4027462996
Ricardo Fernández <ricfdzgon@gmail.com>
2022-02-17 18:19:54 +0100

    Detalles pieza puzzle

--- Assets/Scripts/XRPuzleGrabInteractable.cs
+++ Assets/Scripts/XRPuzleGrabInteractable.cs
@@ -23,16 +23,50 @@ public class XRPuzleGrabInteractable : XRGrabInteractable
             PuzllePieceIdentifier ppiSocket = interactor.gameObject.GetComponentInParent<PuzllePieceIdentifier>();
             if (ppiSocket != null)
             {
-                if (ppi.fila == ppiSocket.fila && ppi.columna - ppiSocket.columna == 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return isSocketCompatible(ppi, ppiSocket, interactor.gameObject.tag);
             }
         }
         return base.IsHoverableBy(interactor);
     }
+
+    public override bool IsSelectableBy(XRBaseInteractor interactor)
+    {
+        if (interactor is XRSocketInteractor)
+        {
+            PuzllePieceIdentifier ppiSocket = interactor.gameObject.GetComponentInParent<PuzllePieceIdentifier>();
+            if (ppiSocket != null)
+            {
+                return isSocketCompatible(ppi, ppiSocket, interactor.gameObject.tag);
+            }
+        }
+        return base.IsSelectableBy(interactor);
+    }
+
+    private bool isSocketCompatible(PuzllePieceIdentifier ppiPropio, PuzllePieceIdentifier ppiSocket, string socketTag)
+    {
+        if (socketTag == "SocketE")
+        {
+            if (ppiPropio.fila == ppiSocket.fila && ppiPropio.columna - ppiSocket.columna == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (ppiPropio.columna == ppiSocket.columna && ppiPropio.fila - ppiSocket.fila == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
 }
